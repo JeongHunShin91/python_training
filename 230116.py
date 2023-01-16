9876671b7567c7955bcff3478e079d4fdd7aaaed
Henry <115703708+JeongHunShin91@users.noreply.github.com>
2023-01-16 21:30:30 +0900

    Add files via upload

---
+++ 230116.py
@@ -0,0 +1,212 @@
+# coding: utf-8
+import numpy as np
+import sys
+sys.path.append('..')
+from dataset import sequence
+
+
+(x_train, t_train), (x_test, t_test) = \
+    sequence.load_data('addition.txt', seed=1984)
+char_to_id, id_to_char = sequence.get_vocab()
+
+print(char_to_id)
+print(x_train.shape, t_train.shape)
+print(x_test.shape, t_test.shape)
+
+print(x_train[0])
+print(t_train[0])
+
+print(''.join([id_to_char[c] for c in x_train[0]]))
+print(''.join([id_to_char[c] for c in t_train[0]]))
+
+import sys
+sys.path.append('..')
+from common.time_layers import *
+from common.base_model import BaseModel
+
+class Encoder:
+    def __init__(self, vocab_size, wordvec_size, hidden_size):
+        V, D, H = vocab_size, wordvec_size, hidden_size
+        rn = np.random.randn
+
+        embed_W = (rn(V, D) / 100).astype('f')
+        lstm_Wx = (rn(D, 4 * H) / np.sqrt(D)).astype('f')
+        lstm_Wh = (rn(H, 4 * H) / np.sqrt(H)).astype('f')
+        lstm_b = np.zeros(4 * H).astype('f')
+
+        self.embed = TimeEmbedding(embed_W)
+        self.lstm = TimeLSTM(lstm_Wx, lstm_Wh, lstm_b, stateful=False)
+
+        self.params = self.embed.params + self.lstm.params
+        self.grads = self.embed.grads + self.lstm.grads
+        self.hs = None
+
+    def forward(self, xs):
+        xs = self.embed.forward(xs)
+        hs = self.lstm.forward(xs)
+        self.hs = hs
+        return hs[:, -1, :]
+
+    def backward(self, dh):
+        dhs = np.zeros_like(self.hs)
+        dhs[:, -1, :] = dh
+
+        dout = self.lstm.backward(dhs)
+        dout = self.embed.backward(dout)
+        return dout
+
+
+class Decoder:
+    def __init__(self, vocab_size, wordvec_size, hidden_size):
+        V, D, H = vocab_size, wordvec_size, hidden_size
+        rn = np.random.randn
+
+        embed_W = (rn(V, D) / 100).astype('f')
+        lstm_Wx = (rn(D, 4 * H) / np.sqrt(D)).astype('f')
+        lstm_Wh = (rn(H, 4 * H) / np.sqrt(H)).astype('f')
+        lstm_b = np.zeros(4 * H).astype('f')
+        affine_W = (rn(H, V) / np.sqrt(H)).astype('f')
+        affine_b = np.zeros(V).astype('f')
+
+        self.embed = TimeEmbedding(embed_W)
+        self.lstm = TimeLSTM(lstm_Wx, lstm_Wh, lstm_b, stateful=True)
+        self.affine = TimeAffine(affine_W, affine_b)
+
+        self.params, self.grads = [], []
+        for layer in (self.embed, self.lstm, self.affine):
+            self.params += layer.params
+            self.grads += layer.grads
+
+    def forward(self, xs, h):
+        self.lstm.set_state(h)
+
+        out = self.embed.forward(xs)
+        out = self.lstm.forward(out)
+        score = self.affine.forward(out)
+        return score
+
+    def backward(self, dscore):
+        dout = self.affine.backward(dscore)
+        dout = self.lstm.backward(dout)
+        dout = self.embed.backward(dout)
+        dh = self.lstm.dh
+        return dh
+
+    def generate(self, h, start_id, sample_size):
+        sampled = []
+        sample_id = start_id
+        self.lstm.set_state(h)
+
+        for _ in range(sample_size):
+            x = np.array(sample_id).reshape((1, 1))
+            out = self.embed.forward(x)
+            out = self.lstm.forward(out)
+            score = self.affine.forward(out)
+
+            sample_id = np.argmax(score.flatten())
+            sampled.append(int(sample_id))
+
+        return sampled
+
+
+class Seq2seq(BaseModel):
+    def __init__(self, vocab_size, wordvec_size, hidden_size):
+        V, D, H = vocab_size, wordvec_size, hidden_size
+        self.encoder = Encoder(V, D, H)
+        self.decoder = Decoder(V, D, H)
+        self.softmax = TimeSoftmaxWithLoss()
+
+        self.params = self.encoder.params + self.decoder.params
+        self.grads = self.encoder.grads + self.decoder.grads
+
+    def forward(self, xs, ts):
+        decoder_xs, decoder_ts = ts[:, :-1], ts[:, 1:]
+
+        h = self.encoder.forward(xs)
+        score = self.decoder.forward(decoder_xs, h)
+        loss = self.softmax.forward(score, decoder_ts)
+        return loss
+
+    def backward(self, dout=1):
+        dout = self.softmax.backward(dout)
+        dh = self.decoder.backward(dout)
+        dout = self.encoder.backward(dh)
+        return dout
+
+    def generate(self, xs, start_id, sample_size):
+        h = self.encoder.forward(xs)
+        sampled = self.decoder.generate(h, start_id, sample_size)
+        return sampled
+
+out = np.arange(20).reshape(1,5,4)
+print(out)
+h = np.arange(3).reshape(1,3)
+print(h)
+hs = np.repeat(h, 5, axis=0).reshape(1, 5, 3)
+print(hs)
+out = np.concatenate((hs, out), axis=2)
+print(out)
+
+# coding: utf-8
+import sys
+sys.path.append('..')
+import numpy as np
+import matplotlib.pyplot as plt
+from dataset import sequence
+from common.optimizer import Adam
+from common.trainer import Trainer
+from common.util import eval_seq2seq
+from seq2seq import Seq2seq
+from peeky_seq2seq import PeekySeq2seq
+
+
+# 데이터셋 읽기
+(x_train, t_train), (x_test, t_test) = sequence.load_data('addition.txt')
+char_to_id, id_to_char = sequence.get_vocab()
+
+# 입력 반전 여부 설정 =============================================
+is_reverse =  False
+# if is_reverse:
+#     x_train, x_test = x_train[:, ::-1], x_test[:, ::-1]
+# ================================================================
+
+# 하이퍼파라미터 설정
+vocab_size = len(char_to_id)
+print(vocab_size)
+print(char_to_id)
+wordvec_size = 16
+hideen_size = 128
+batch_size = 128
+max_epoch = 25
+max_grad = 5.0
+
+# 일반 혹은 엿보기(Peeky) 설정 =====================================
+model = Seq2seq(vocab_size, wordvec_size, hideen_size)
+# model = PeekySeq2seq(vocab_size, wordvec_size, hideen_size)
+# ================================================================
+optimizer = Adam()
+trainer = Trainer(model, optimizer)
+
+acc_list = []
+for epoch in range(max_epoch):
+    trainer.fit(x_train, t_train, max_epoch=1,
+                batch_size=batch_size, max_grad=max_grad)
+
+    correct_num = 0
+    for i in range(len(x_test)):
+        question, correct = x_test[[i]], t_test[[i]]
+        verbose = i < 10
+        correct_num += eval_seq2seq(model, question, correct,
+                                    id_to_char, verbose, is_reverse)
+
+    acc = float(correct_num) / len(x_test)
+    acc_list.append(acc)
+    print('검증 정확도 %.3f%%' % (acc * 100))
+
+# 그래프 그리기
+x = np.arange(len(acc_list))
+plt.plot(x, acc_list, marker='o')
+plt.xlabel('에폭')
+plt.ylabel('정확도')
+plt.ylim(0, 1.0)
+plt.show()
